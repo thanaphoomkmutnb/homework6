29ca36b3473d029881d3c515e832004b99609daf
Thanaphoom ArunChit <67995808+thanaphoomkmutnb@users.noreply.github.com>
2020-09-18 20:58:09 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,23 +4,14 @@
 #include <string>
 
 using namespace std;
-int Guess(int num);
+int Guess(int random);
 
 int main() {
     srand(time(NULL));
-    int random = rand() % 9 + 1, number = 0, Round = 0;
+    int random = rand() % 9 + 1, Round = 0;
     cout << "###Welcome to guessing number game###\n";
     cout << "Secret number has been chosen\n";
-    do {
-        cout << "Guess the number (1 to 10): ";
-        cin >> number;
-        if (number > random) {
-            cout << "The secret number is higher\n";
-        } else if (number < random) {
-            cout << "The secret number is lower\n";
-        }
-        Round = Guess(Round);
-    } while (random != number);
+    Round = Guess(random);
     cout << "Congratulations!\n";
     cout << "The secret number is " << random << endl;
     cout << "You made " << Round << " guesses" << endl;
@@ -29,6 +20,17 @@ int main() {
 }
 
 
-int Guess(int num) {
-    return num+1;
+int Guess(int random) {
+	int Round = 0, number = 0;
+    do {
+        cout << "Guess the number (1 to 10): ";
+        cin >> number;
+        if (number > random) {
+            cout << "The secret number is higher\n";
+        } else if (number < random) {
+            cout << "The secret number is lower\n";
+        }
+        Round++;
+    } while (random != number);
+	return Round;
 }
